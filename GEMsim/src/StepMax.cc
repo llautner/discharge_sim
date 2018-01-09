a1ee91b17299a1d1007fd672912e997d6f493a44
Andreas Mathis <andreas.mathis@cern.ch>
2018-01-09 15:18:17 +0100

    Set max step length to 5 micron

--- GEMsim/src/StepMax.cc
+++ GEMsim/src/StepMax.cc
@@ -9,8 +9,8 @@
 StepMax::StepMax (const G4String& processName) :   G4VDiscreteProcess(processName), stepLimit(110000000)    {
   SetProcessType(fGeneral);
   SetProcessSubType(401);
-//   stepLimit = 0.005 *CLHEP::mm;
-  stepLimit = 1*CLHEP::mm;
+  stepLimit = 0.005 *CLHEP::mm;
+//  stepLimit = 1*CLHEP::mm;
 
 }
 
